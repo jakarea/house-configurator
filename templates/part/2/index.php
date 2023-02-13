1a546a5ae2580a3f796bfb6ec929c843db6a5120
Md Arif <arifypp@gmail.com>
2023-02-13 15:40:47 +0600

    ai system confguration

--- templates/part/2/index.php
+++ templates/part/2/index.php
@@ -8,16 +8,16 @@
 }
 
 /**
-* Plugin Name:       House Configurator
+* Plugin Name:      House Configurator
 * Plugin URI:		http://giopio.com
-* Description:       House Configurator is helping for making house estimate in a minutes.
-* Version:           1.0.0
+* Description:      House Configurator is helping for making house estimate in a minutes.
+* Version:          1.0.0
 */
 
 /**
  * The code that runs during show success message or error message
  */
-if ( isset( $_GET['message'] ) && $_GET['message'] == 1 ) {
+if ( isset( $_GET['message'] ) && $_GET['message'] == 'success' ) {
     echo '<div class="notice notice-success is-dismissible">
             <p>Data has been added successfully!</p>
         </div>';
@@ -29,18 +29,18 @@ if ( isset( $_GET['message'] ) && $_GET['message'] == 1 ) {
     echo '<div class="notice notice-success is-dismissible">
             <p>Data has been deleted successfully!</p>
         </div>';
-} else if ( isset( $_GET['message'] ) && $_GET['message'] == 4 ) {
+} else if ( isset( $_GET['message'] ) && $_GET['message'] == 'error' ) {
     echo '<div class="notice notice-error is-dismissible">
-            <p>Data has been deleted failed!</p>
+            <p>Something went wrong!</p>
         </div>';
 }
-    
-/**
-* Import level template
-*/
-require_once 'levels.php';
 
 /**
- * import feature template
+* Import Where you hear us template
+*/
+require_once 'construction-contact.php';
+
+/**
+ * Import BTW template
  */
-require_once 'features.php';
+require_once 'construction-choice.php';
