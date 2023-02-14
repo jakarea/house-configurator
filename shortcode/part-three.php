5445f1c77e94d727c5a006a3a0c3f646d2d4a7aa
Md Arif <arifypp@gmail.com>
2023-02-14 15:06:35 +0600

    Part three started

---
+++ shortcode/part-three.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * @package  HouseConfigurator
+ */
+?>
+
+<!-- add bootstrap card first 8 then last 4 in row -->
+<div class="container">
+    <div class="row">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">
+                    Featured
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-header">
+                    Featured 2
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
