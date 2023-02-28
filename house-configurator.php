524322b12aa29b16b0e788f1276ebe56946d70ee
Md Arif <arifypp@gmail.com>
2023-02-28 17:50:04 +0600

    update part four settings

--- house-configurator.php
+++ house-configurator.php
@@ -1,43 +1,29 @@
 <?php
 /**
- * @package  HouseConfigurator
+ * Plugin Name: House Configurator
+ * Plugin URI: http://giopio.com
+ * Description: House Configurator is helping for making house estimate in a minutes.
+ * Version: 1.0.0
+ * Author: Giopio
+ * Author URI: http://happyarif.com
+ * License: GPLv2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: house-configurator
+ * Domain Path: /languages
  */
-/*
-Plugin Name: House Configurator
-Plugin URI: http://giopio.com
-Description: House Configurator is helping for making house estimate in a minutes.
-Version: 1.0.0
-Author: Giopio
-Author URI: http://happyarif.com
-License: GPLv2 or later
-Text Domain: house-configurator
-*/
 
-/*
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-
-Copyright 2005-2015 Automattic, Inc.
-*/
-
-// If this file is called firectly, abort!!!
+/* If this file is called firectly, abort!!! */
 defined( 'ABSPATH' ) or die( 'Hey, what are you doing here? You silly human!' );
 
-// Require once the Composer Autoload
+/* Require once the Composer Autoload */
 if ( file_exists( dirname( __FILE__ ) . '/vendor/autoload.php' ) ) {
 	require_once dirname( __FILE__ ) . '/vendor/autoload.php';
 }
+
+use Inc\Base\Activate;
+use Inc\Base\Deactivate;
+use Inc\Init;
+
 /**
  * Create table when plugin is activated
  */
@@ -56,6 +42,31 @@ function house_configurator_install() {
 	) $charset_collate;";
 	require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
 	dbDelta( $sql );
+
+	// create a new table for type [name, price]
+	$charset_collateh = $wpdb->get_charset_collate();
+	$house_type = $wpdb->prefix . 'house_configurator_type';
+	$sql1 = "CREATE TABLE $house_type (
+		id mediumint(9) NOT NULL AUTO_INCREMENT,
+		name tinytext NOT NULL,
+		price float NOT NULL,
+		PRIMARY KEY  (id)
+	) $charset_collateh;";
+	require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+	dbDelta( $sql1 );
+
+	// create a new table for feature list [name, price, type_id in array]
+	$charset_collatef = $wpdb->get_charset_collate();
+	$house_feature = $wpdb->prefix . 'house_configurator_feature';
+	$sql2 = "CREATE TABLE $house_feature (
+		id mediumint(9) NOT NULL AUTO_INCREMENT,
+		name tinytext NOT NULL,
+		price float NOT NULL,
+		type_id tinytext NOT NULL,
+		PRIMARY KEY  (id)
+	) $charset_collatef;";
+	require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+	dbDelta( $sql2 );
 }
 register_activation_hook( __FILE__, 'house_configurator_install' );
 
@@ -69,15 +80,15 @@ include_once( 'inc/function.php' );
  * The code that runs during plugin activation
  */
 function activate_house_configurator() {
-	Inc\Base\Activate::activate();
+	Activate::activate();
 }
-register_activation_hook( __FILE__, 'activate_house_configurator' );
+register_activation_hook( plugin_dir_path( __FILE__ ), 'activate_house_configurator' );
 
 /**
  * The code that runs during plugin deactivation
  */
 function deactivate_house_configurator() {
-	Inc\Base\Deactivate::deactivate();
+	Deactivate::deactivate();
 }
 register_deactivation_hook( __FILE__, 'deactivate_house_configurator' );
 
@@ -85,5 +96,13 @@ register_deactivation_hook( __FILE__, 'deactivate_house_configurator' );
  * Initialize all the core classes of the plugin
  */
 if ( class_exists( 'Inc\\Init' ) ) {
-	Inc\Init::register_services();
+	Init::register_services();
 }
+
+/**
+ * Text Domain
+ */
+function house_configurator_load_textdomain() {
+	load_plugin_textdomain( 'house-configurator', false, dirname( plugin_basename( __FILE__ ) ) . '/languages' );
+}
+add_action( 'plugins_loaded', 'house_configurator_load_textdomain' );
